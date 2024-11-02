bbfd0c8877b4742cf449b5caba4cf28ba6b6e04e
Fco Javier Garrido <francisco.javier.garrido@hotmail.com>
2024-11-03 00:29:35 +0100

    Creado String cadena a mostrar y metodo toString

--- src/main/resources/Direccion.java
+++ src/main/resources/Direccion.java
@@ -1,6 +1,15 @@
 public enum Direccion {
-    NORESTE="Noreste",
-    SURESTE="Sureste",
-    SUROESTE="Suroeste",
-    NOROESTE="Noroeste"
+    NORESTE("Noreste"),
+    SURESTE("Sureste"),
+    SUROESTE("Suroeste"),
+    NOROESTE("Noroeste");
+
+    private String cadenaAMostrar;
+
+    @Override
+    public String toString() {
+        return "Direccion{" +
+                "cadenaAMostrar='" + cadenaAMostrar + '\'' +
+                '}';
+    }
 }
